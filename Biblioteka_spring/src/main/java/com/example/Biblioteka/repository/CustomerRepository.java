ea65869cc99548287658ade247295a132d1056e3
cingurpl <lilnorbi2115@gmail.com>
2024-12-29 03:16:53 +0100

    wszystko skonfigurowane mniej wiecej

--- Biblioteka_spring/src/main/java/com/example/Biblioteka/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Biblioteka.repository;
-
-import com.example.Biblioteka.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
